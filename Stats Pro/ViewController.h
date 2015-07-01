2fc79f49a290ba6e5004ab2a55c785ad5104459a
Daniyar Mukhanov <mukhanov.daniyar@gmail.com>
2015-07-01 14:24:17 +0600

    Initial Commit

---
+++ Stats Pro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stats Pro
+//
+//  Created by Daniyar Mukhanov on 7/1/15.
+//  Copyright (c) 2015 Daniyar Mukhanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Stats Pro/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Stats Pro
+//
+//  Created by Daniyar Mukhanov on 7/1/15.
+//  Copyright (c) 2015 Daniyar Mukhanov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
