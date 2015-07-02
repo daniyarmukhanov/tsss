7c9cba14f4852f5e1cf7d24c207d42d8ed6fcb1b
Daniyar Mukhanov <mukhanov.daniyar@gmail.com>
2015-07-02 16:53:18 +0600

    name to title in teams

--- Stats Pro/Team.h
+++ Stats Pro/Team.h
@@ -11,7 +11,7 @@
 
 @interface Team : NSObject
 
-@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *title;
 @property (nonatomic, strong) NSMutableArray *scored;
 @property (nonatomic, strong) NSMutableArray *conceded;
 @property (nonatomic, strong) NSMutableArray *corners;
