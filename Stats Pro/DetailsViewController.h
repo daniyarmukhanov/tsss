476131a8fdfb2c69ce2f2b4165e8b158c620dda5
Daniyar Mukhanov <mukhanov.daniyar@gmail.com>
2015-07-06 18:05:18 +0600

    none

---
+++ Stats Pro/DetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailsViewController.h
+//  Stats Pro
+//
+//  Created by Daniyar Mukhanov on 7/6/15.
+//  Copyright (c) 2015 Daniyar Mukhanov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface DetailsViewController : ViewController
+
+@end
